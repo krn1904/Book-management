d651842b7f662075c157361b70eedc9f8049ba28
Karan Soni <Karanssoni2002@gmail.com>
2023-08-23 18:18:35 +0530

    backup the code

---
+++ BE/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+const cors = require("cors");
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//connect to mongoose
+mongoose.connect("mongodb://localhost/bookapp", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+const con = mongoose.connection;
+
+//routes
+const userRouter = require("./routes/users");
+const bookRouter = require("./routes/books");
+const cartRouter = require("./routes/cart");
+
+app.use("/api", userRouter);
+app.use("/api", bookRouter);
+app.use("/api", cartRouter);
+
+con.on("open", () => {
+  console.log("connection established");
+});
+app.listen(2000, () => {
+  console.log("server listening on 2000");
+});
